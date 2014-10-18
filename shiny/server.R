79cd031f54d24e710969b800a698aa0f804d407e
jaeddy <james.a.eddy@gmail.com>
2014-10-18 16:25:23 -0700

    created template files for shiny app

---
+++ shiny/server.R
@@ -0,0 +1,7 @@
+library(shiny)
+library(GSReg)
+library(rCharts)
+
+shinyServer(function(input, output, session) {
+
+})
---
+++ shiny/ui.R
@@ -0,0 +1,9 @@
+library(shiny)
+library(GSReg)
+library(rCharts)
+
+shinyUI(fluidPage(
+    
+    titlePanel("diracViz App"),
+
+))
