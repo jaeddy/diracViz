8b62fd171d300c01faa9940b71f3f58b3d48b72b
jaeddy <james.a.eddy@gmail.com>
2014-10-21 22:48:36 -0700

    data load and dirac results working

--- shiny/server.R
+++ shiny/server.R
@@ -11,16 +11,71 @@ data(GSBenchMarkDatasets)
 shinyServer(function(input, output, session) {
 
     output$currentData <- renderText({
-        input$data
+        input$dataset
     })
     
-    getData <- observe({
+    data <- reactiveValues()
+    
+    # Load and summarize the selected dataset
+    output$value <- renderTable({
+        input$load
+        
         if (input$load > 0) {
-            dataSetName <- input$data
+            dataSetName <- isolate(input$dataset)
             load_data(dataSetName)
             exprsdata <- clean_data(exprsdata)
+            
+            data$exprsdata <- exprsdata
+            data$phenotypes <- phenotypes
+            data$phenotypesLevels <- phenotypesLevels
+            
             df <- summarize_data(exprsdata, phenotypes, phenotypesLevels)
-            output$value <- renderTable({ df })
+            df
+        }       
+    })
+    
+    output$size <- renderText({
+        input$load
+        
+        if (input$load > 0) {
+            exprsdata <- data$exprsdata
+            nrow(exprsdata)
+        }
+    })
+    
+    runDirac <- observe({
+        if (input$run > 0) {
+            Nperm <- input$Nperm
+            minGeneNum <- input$minGeneNum
+            exprsdata <- data$exprsdata
+            phenotypes <- data$phenotypes
+            
+            diracResult <- GSReg.GeneSets.DIRAC(exprsdata, 
+                                                diracpathways, phenotypes, 
+                                                Nperm, minGeneNum)
+            
+            sigPathways <- which(diracResult$pvalues < 0.05)
+            dysregulatedPathways <- rbind(diracResult$mu1[sigPathways],
+                                          diracResult$mu2[sigPathways], 
+                                          diracResult$pvalues[sigPathways])
+
+            rownames(dysregulatedPathways) <- c("mu1", "mu2", "pvalue")
+            
+            data$results <- t(dysregulatedPathways)
+        }  
+    })
+    
+    output$topPathways <- renderDataTable({
+        input$run
+        
+        if (input$run > 0) {
+            results <- isolate(data$results)
+            pathwayTable <- data.frame(results)
+            pathwayTable <- cbind(pathway = row.names(pathwayTable),
+                                  pathwayTable)
+            pathwayTable$mu1 <- 1 - pathwayTable$mu1
+            pathwayTable$mu2 <- 1 - pathwayTable$mu2
+            pathwayTable
         }
     })
 })
--- shiny/ui.R
+++ shiny/ui.R
@@ -12,25 +12,58 @@ shinyUI(fluidPage(
     
     fluidRow(
         column(4,
-               h3("Load Data"),
-               selectInput("data",
-                           "Select data set:",
-                           GSBenchMark.Dataset.names,
-                           selectize = TRUE),
-               actionButton("load",
-                            "Load data"),
-               hr(),
-               fluidRow(
-                   tableOutput("value")
+               # Controls for selecting data and parameters
+               h3("Control Panel"),
+               wellPanel(
+                   # Select and load expression dataset
+                   fluidRow(
+                       h4("Data Selection"),
+                       selectizeInput("dataset",
+                                   NULL,
+                                   choices = GSBenchMark.Dataset.names,
+                                   options = list(
+                                       placeholder = "Select dataset...",
+                                       onInitialize = 
+                                           I(paste0('function()',
+                                                    '{ this.setValue(""); }'))
+                                   )
+                       ),
+                       actionButton("load",
+                                    "Load data")
+                   ),
+                   hr(),
+                   # Preview dataset
+                   fluidRow(
+                       h4("Data Summary"),
+                       tableOutput("value")
+                   ),
+                   hr(),
+                   # Set parameters for DIRAC
+                   fluidRow(
+                       h4("DIRAC Settings"),
+                       sliderInput("Nperm", 
+                                   "Num. permutations:",
+                                   min = 1, max = 10000, value = 1),
+                       br(),
+                       sliderInput("minGeneNum", 
+                                   "Min. num. genes per pathway",
+                                   min = 3, max = 15, value = 5),
+                       br(),
+                       actionButton("run",
+                                    "Run DIRAC")
                    )
-               ),
+               )
+        ),
         
         column(8,
                h3("DIRAC Pathway Analysis"),
                tabsetPanel(
                    tabPanel("Results",
                             h4("Top pathways..."),
-                            textOutput("currentData")
+                            textOutput("currentData"),
+                            textOutput("size"),
+                            br(),
+                            dataTableOutput("topPathways")
                             ),
                    tabPanel("Pathway Viz"
                             ),
